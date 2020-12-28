75d4f3fcc45084a93880dd7a480f48cb546f0b51
Ricky Rhodes <ricky@rickyrhodes.com>
2020-12-28 17:05:33 -0500

    add completed tutorial

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,14 @@ import Decentragram from '../abis/Decentragram.json';
 import Navbar from './Navbar';
 import Main from './Main';
 
+// Connect to IPFS
+const ipfsClient = require('ipfs-http-client');
+const ipfs = ipfsClient({
+  host: 'ipfs.infura.io',
+  port: 5001,
+  protocol: 'https',
+});
+
 // Web3 is the main JS library for interacting with Ethereum
 // Turns our clientside app into a blockchain app
 // Metamask turns our browser into a blockchain browser
@@ -49,15 +57,82 @@ class App extends Component {
         Decentragram.abi,
         networkData.address
       );
+      this.setState({ decentragram });
+      // We need to use .call() because we are using web3js to access the blockchain
+      const imagesCount = await decentragram.methods.imageCount().call();
+      this.setState({ imagesCount });
+
+      // Load Images from Blockchain
+      for (let i = 1; i <= imagesCount; i++) {
+        const image = await decentragram.methods.images(i).call();
+        this.setState({ images: [...this.state.images, image] });
+      }
+
+      // Sort images, shows highest tipped images first
+      this.setState({
+        images: this.state.images.sort((a, b) => b.tipAmount - a.tipAmount),
+      });
+
+      this.setState({ loading: false });
     } else {
       window.alert('Decentragram contract not deployed to detected network.');
     }
   }
 
+  captureFile = event => {
+    event.preventDefault();
+
+    // Pre-process the file so it can be uploaded to IPFS
+    // Read file off target
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) });
+      console.log('buffer', this.state.buffer);
+    };
+  };
+
+  uploadImage = description => {
+    console.log('Submitting file to IPFS....');
+
+    // Adding file to the IPFS
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log('IPFS result', result);
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      this.setState({ loading: true });
+      // Calling the smart contract function to load it to the blockchain
+      this.state.decentragram.methods
+        .uploadImage(result[0].hash, description)
+        .send({ from: this.state.account })
+        .on('transactionHash', hash => {
+          this.setState({ loading: false });
+        });
+    });
+  };
+
+  tipImageOwner = (id, tipAmount) => {
+    this.setState({ loading: true });
+    this.state.decentragram.methods
+      .tipImageOwner(id)
+      .send({ from: this.state.account, value: tipAmount })
+      .on('transactionHash', hash => {
+        this.setState({ loading: false });
+      });
+  };
+
   constructor(props) {
     super(props);
     this.state = {
       account: '',
+      decentragram: null,
+      images: [],
+      loading: true,
     };
   }
 
@@ -71,7 +146,10 @@ class App extends Component {
           </div>
         ) : (
           <Main
-          // Code...
+            captureFile={this.captureFile}
+            uploadImage={this.uploadImage}
+            images={this.state.images}
+            tipImageOwner={this.tipImageOwner}
           />
         )}
         }
