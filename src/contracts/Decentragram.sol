0e08081a2f2a5534c58b303dc4a89de6a111cb86
Ricky Rhodes <ricky@rickyrhodes.com>
2020-12-27 20:18:03 -0500

    add mapping and struct Image

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -4,5 +4,29 @@ contract Decentragram {
   // Will be stored on blockchain because it is a state variable
   // Must tell solidity it is a string since it is a statically typed language
   // Public means we can read this variable outside of this smart contract
-  string public name = "Decentragam";
+  string public name = "Decentragram";
+
+
+// Store Images
+
+  // Mapping data type works like a database (or hash table)
+  // A key value pair. key is the id, value gets stored on blockchain
+  // uint = key, a number (unassigned integer, cant be negative)
+  // Image = IPFS hash
+  mapping(uint => Image) public images;
+
+  // Struct allows you to create your own datatype
+  // Like a table in a database, the variables are the columns
+  struct Image {
+    uint id;
+    string hash;
+    string description;
+    uint tipAmount;
+    address payable author;
+  }
+
+
+  // Create Images
+
+  // Tip Images
 }
